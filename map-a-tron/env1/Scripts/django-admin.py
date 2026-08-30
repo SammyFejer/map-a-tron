bc226c74c152cefe54201f2200b8030ab3f76a47
SammyFejer <0348257@schoolsnet.act.edu.au>
2026-08-30 15:31:40 +1000

    hgfjkdg

---
+++ map-a-tron/env1/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\sammy\source\repos\SammyFejer\map-a-tron\map-a-tron\env1\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
