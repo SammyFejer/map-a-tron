57fc5764ee50796a0a8b08b99cb6fd7e4de9e8b9
SammyFejer <0348257@schoolsnet.act.edu.au>
2026-09-01 18:08:13 +1000

    two geotiffs

--- map-a-tron/assets/GeoTIF.js
+++ map-a-tron/assets/GeoTIF.js
@@ -23,27 +23,47 @@
 //});
 
 import Map from 'ol/Map.js';
-import { getView, withExtentCenter, withHigherResolutions } from 'ol/View.js';
+import View from 'ol/View.js';
 import TileLayer from 'ol/layer/WebGLTile.js';
 import GeoTIFF from 'ol/source/GeoTIFF.js';
 
-const source = new GeoTIFF({
+const source1 = new GeoTIFF({
+    sources: [{ url: 'https://storage.googleapis.com/hugemunous_file/Corang_COG.tif' }],
+});
+
+const source2 = new GeoTIFF({
     sources: [
-        {
-            url: 'https://storage.googleapis.com/hugemunous_file/Corang_COG.tif',
-        },
+
+        
+        { url: 'https://storage.googleapis.com/hugemunous_file/Endrick_COG.tif' },
+
     ],
 });
 
+
+
+const layer1 = new TileLayer({
+    source: source1,
+});
+
+const layer2 = new TileLayer({
+    source: source2,
+});
+
+const view = new View({ center: [16696162.166265408, -4196098.137680702], zoom: 13, });
+
+
 const map = new Map({
     target: 'map',
-    layers: [
-        new TileLayer({
-            source: source,
-        }),
-    ],
-     view: getView(source, withHigherResolutions(1), withExtentCenter()),
-   /*view: new Map().getView(),*/
+    layers: [layer1, layer2],
+    view: view,
+   //  view: source1.getView(/*(source, withHigherResolutions(1), withExtentCenter()*/),
+   ///*view: new Map().getView(),*/
+});
+
+map.on('click', function (event) {
+    const coord = event.coordinate;
+    console.log('Clicked coordinates:', coord);
 });
 
 
