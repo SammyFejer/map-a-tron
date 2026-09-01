f2ed5b76e2391b3bd29824397362084ccb68ab4f
SammyFejer <0348257@schoolsnet.act.edu.au>
2026-09-01 18:08:30 +1000

    uy

--- testingsjkndf.py
+++
@@ -1,7 +0,0 @@
-from cv2 import imshow
-import numpy as np  
-img = cv2.imread('8927-3N_Corang_GetlostMap_V15.tif', cv2.IMREAD_UNCHANGED)
-cv2.imshow=('yahoo', img)
-cv2.waitKey(0)
-cv2.distroyAllWindows()
-
